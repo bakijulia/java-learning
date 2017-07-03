9805fdad3653a2860f2e2cb833411301b71a148a
Julia Baki <bakijulia@gmail.com>
2017-07-03 20:05:48 +0200

    Hello java

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,5 +4,14 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello, World!");
+
+        int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myResult = 1000 - myTotal;
+
+        System.out.println(myResult);
     }
 }
